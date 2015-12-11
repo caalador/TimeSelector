e53dbc5b5d3ddfa35ae06c90fac2aa025f56c5c0
Mikael Grankvist <mikael.grankvist@vaadin.com>
2015-12-11 10:40:12 +0200

    Formatting.

--- TimeSelector-demo/src/main/java/org/percepta/mgrankvi/demo/DemoUI.java
+++ TimeSelector-demo/src/main/java/org/percepta/mgrankvi/demo/DemoUI.java
@@ -1,28 +1,25 @@
 package org.percepta.mgrankvi.demo;
 
-import com.vaadin.server.DeploymentConfiguration;
-import com.vaadin.server.ServiceException;
-import com.vaadin.server.VaadinServletService;
-import org.percepta.mgrankvi.TimeSelector;
-
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.DeploymentConfiguration;
+import com.vaadin.server.ServiceException;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.server.VaadinServletService;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import org.percepta.mgrankvi.TimeSelector;
 
+import javax.servlet.annotation.WebServlet;
 import java.util.Date;
 
 @Theme("demo")
 @Title("MyComponent Add-on Demo")
 @SuppressWarnings("serial")
-public class DemoUI extends UI
-{
+public class DemoUI extends UI {
 
     @WebServlet(value = "/*", asyncSupported = true)
     @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class, widgetset = "org.percepta.mgrankvi.demo.DemoWidgetSet")
@@ -38,13 +35,13 @@ public class DemoUI extends UI
 
         // Initialize our new UI component
         final TimeSelector component = new TimeSelector();
-component.addSelectionChangeListener(new TimeSelector.SelectionChangeListener() {
+        component.addSelectionChangeListener(new TimeSelector.SelectionChangeListener() {
 
-    @Override
-    public void selectionChanged(TimeSelector.SelectionChangeEvent event) {
-        System.out.println(event.getHours() + ":" + event.getMinutes());
-    }
-});
+            @Override
+            public void selectionChanged(TimeSelector.SelectionChangeEvent event) {
+                System.out.println(event.getHours() + ":" + event.getMinutes());
+            }
+        });
 
         Date time = new Date();
         component.setTime(time.getHours(), time.getMinutes());
